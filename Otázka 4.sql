c4eeeb62cb5899ddaaf3698ebccf05930ded9b6d
HanaSimkova <156844216+HanaSimkova@users.noreply.github.com>
2024-03-09 10:50:00 +0100

    Update Otázka 4.sql

--- Otázka 4.sql
+++ Otázka 4.sql
@@ -4,11 +4,11 @@ SELECT
 	round(avg(th.wage)) AS avg_wage, 
 	round((avg(th.price) - lag(avg(th.price)) OVER (ORDER BY th.payroll_year)) / lag(avg(th.price)) 
  	OVER (ORDER BY th.payroll_year) * 100, 2) AS price_increase_percentage, 
-    round((avg(th.wage) - lag(avg(th.wage)) OVER (ORDER BY th.payroll_year)) / lag(avg(th.wage)) 
-    OVER (ORDER BY th.payroll_year) * 100, 2) AS value_increase_percentage, 
-    round((avg(th.price) - lag(avg(th.price)) OVER (ORDER BY th.payroll_year)) / lag(avg(th.price)) 
-    OVER (ORDER BY th.payroll_year) * 100, 2) - round((avg(th.wage) - lag(avg(th.wage)) OVER (ORDER BY th.payroll_year)) / lag(avg(th.wage)) 
-    OVER (ORDER BY th.payroll_year) * 100, 2) AS difference_percentage
+	round((avg(th.wage) - lag(avg(th.wage)) OVER (ORDER BY th.payroll_year)) / lag(avg(th.wage)) 
+	OVER (ORDER BY th.payroll_year) * 100, 2) AS value_increase_percentage, 
+	round((avg(th.price) - lag(avg(th.price)) OVER (ORDER BY th.payroll_year)) / lag(avg(th.price)) 
+	OVER (ORDER BY th.payroll_year) * 100, 2) - round((avg(th.wage) - lag(avg(th.wage)) OVER (ORDER BY th.payroll_year)) / lag(avg(th.wage)) 
+	OVER (ORDER BY th.payroll_year) * 100, 2) AS difference_percentage
 FROM t_hana_simkova_project_sql_primary_final th
 WHERE th.payroll_year BETWEEN 2006 AND 2018
 GROUP BY th.payroll_year
